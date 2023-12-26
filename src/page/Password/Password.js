cd0cde833a680415534e196fcc41c063471c9af7
insu3160 <is23193160@gmail.com>
2023-12-26 21:42:42 +0900

    회원가입과 비밀번호 찾기에 checkedMessage 추가

--- src/page/Password/Password.js
+++ src/page/Password/Password.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { View, Text, TextInput, StyleSheet, Image, Pressable, SafeAreaView} from 'react-native';
+import { View, Text, TextInput, StyleSheet, Image, Pressable, SafeAreaView, TouchableOpacity} from 'react-native';
 import { Dimensions } from 'react-native';
 import GuideModal from '../Modal/GuideModal';
 import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
@@ -19,9 +19,10 @@ const Password = ({ navigation }) => {
   const [emailCheckNumber, setemailCheckNumber] = useState('');
   const [emailCheckError, setEmailCheckError] = useState(''); 
   const [isEmailNumberValid,setIsEmailNumberValid] = useState(false);
-  const [nickname, setnickname] = useState('');
-  const [nickNameCheckError,setNickNameCheckError] = useState('');
-  const [isNickNameValid,setIsNickNameValid] = useState(false);
+  const [studentIdCheckSuccess, setStudentIdCheckSuccess] = useState('');
+  const [emailCheckSuccess, setEmailCheckSuccess] = useState(''); 
+  const [passwordSuccess,setPasswordSuccess] = useState('');
+  const [passwordConfirmationSuccess, setPasswordConfirmationSuccess] = useState('');
   const [pwd, setpwd] = useState('');
   const [passwordError,setPasswordError] = useState('');
   const [passwordConfirmation, setPasswordConfirmation] = useState('');
@@ -32,43 +33,50 @@ const Password = ({ navigation }) => {
 
   const handleStudentId = async () => {
     const studentIdPattern = /^\d{9}$/;
-  
     if (!studentIdPattern.test(studentId)) {
       setStudentIdCheckError('유효한 학번을 입력해주세요.');
       setIsEmailVerified(false);
       setIsStudentIdValid(false);
       setEmailCheckError('');
+      setStudentIdCheckSuccess('');
     } else {
-      const apiUrl = 'https://192.168.0.3:8080/mail';
-      try {
-        // 학번을 백엔드로 전송
-        const response = await axios.post(apiUrl, {
-          studentId,
-        });
+      setIsEmailVerified(true);
+      setIsStudentIdValid(true);
+      setStudentIdCheckError('');
+      setStudentIdCheckSuccess('인증번호가 전송되었습니다.');
+    //  const apiUrl = 'https://192.168.0.3:8080/mail';
+    //   try {
+    //     // 학번을 백엔드로 전송
+    //     const response = await axios.post(apiUrl, {
+    //       studentId,
+    //     });
   
-        if (response.status===200) {
-          // 성공적으로 이메일을 보낸 경우, 이메일 인증이 필요하다는 메시지를 표시할 수 있습니다.
-          setIsEmailVerified(true);
-          setIsStudentIdValid(true);
-          setStudentIdCheckError('');
-        } else {
-          // 백엔드에서 실패한 경우, 에러 메시지 표시 또는 적절한 조치 수행
-          setStudentIdCheckError(response.data.message || '이메일 전송에 실패했습니다.');
-          setIsEmailVerified(false);
-          setIsStudentIdValid(false);
-          setEmailCheckError('');
-        }
-      } catch (error) {
-        console.error('이메일 전송 실패:', error);
-        setStudentIdCheckError('이메일 전송에 실패했습니다. 다시 시도해주세요.');
-        setIsEmailVerified(true);
-          setIsStudentIdValid(true);
-          setStudentIdCheckError('');
-      }
+    //     if (response.status===200) {
+    //       // 성공적으로 이메일을 보낸 경우, 이메일 인증이 필요하다는 메시지를 표시할 수 있습니다.
+    //       setIsEmailVerified(true);
+    //       setIsStudentIdValid(true);
+    //       setStudentIdCheckError('');
+    //     } else {
+    //       // 백엔드에서 실패한 경우, 에러 메시지 표시 또는 적절한 조치 수행
+    //       setStudentIdCheckError(response.data.message || '이메일 전송에 실패했습니다.');
+    //       setIsEmailVerified(false);
+    //       setIsStudentIdValid(false);
+    //       setEmailCheckError('');
+    //       setEmailCheckSuccess('학번 인증이 완료되었습니다.');
+    //     }
+    //   } catch (error) {
+    //     console.error('이메일 전송 실패:', error);
+    //     setStudentIdCheckError('이메일 전송에 실패했습니다. 다시 시도해주세요.');
+    //     setIsEmailVerified(true);
+    //       setIsStudentIdValid(true);
+    //       setStudentIdCheckError('');
+    //   }
+    // }
     }
   };
 
   const handleEmailCheck = async () => {
+    setEmailCheckSuccess('');
     if(isEmailVerified){
       if(emailCheckNumber != 666){
         setEmailCheckError('인증번호가 일치하지 않습니다.');
@@ -77,6 +85,7 @@ const Password = ({ navigation }) => {
       else{
         setEmailCheckError('')
         setIsEmailNumberValid(true);
+        setEmailCheckSuccess('인증번호가 일치합니다.');
       }
     }
     else {
@@ -87,24 +96,27 @@ const Password = ({ navigation }) => {
 
   const handlePasswordChange = (text) => {
     const passwordPattern = /^(?=.*[a-z])(?=.*\d)(?=.*[!@#$%^&*()_+])[a-z\d!@#$%^&*()_+]{8,}$/;
-
     if (!passwordPattern.test(text)) {
       setPasswordError('비밀번호는 영문, 숫자, 기호를 포함하고 8자리 이상이어야 합니다.');
+      setPasswordSuccess('');
     }
     else {
       setPasswordError('');
+      setPasswordSuccess('사용가능한 비밀번호입니다.');
     }
 
     setpwd(text);
   };
 
   const handlePasswordConfirmationChange = (text) => {
+    setPasswordConfirmationSuccess('');
     if (text !== pwd) {
       setPasswordConfirmationError('비밀번호가 일치하지 않습니다.');
       setIsPasswordValid(false);
     } else {
       setPasswordConfirmationError('');
       setIsPasswordValid(true);
+      setPasswordConfirmationSuccess('비밀번호가 일치합니다.');
     }
 
     setPasswordConfirmation(text);
@@ -116,9 +128,6 @@ const Password = ({ navigation }) => {
       }
       else if(!isEmailNumberValid){
         setSignupCheckError('올바른 인증번호를 입력해주세요.');
-      }
-      else if(!isNickNameValid){
-        setSignupCheckError('올바른 닉네임을 입력해주세요.');
       }
       else if(passwordError){
         setSignupCheckError('올바른 비밀번호를 입력해주세요.');
@@ -180,9 +189,9 @@ const Password = ({ navigation }) => {
                     setIsEmailVerified(false);
                   }}
                 />
-                <Pressable style={[styles.checkBox,styles.marginLeft12]} onPress={handleStudentId}>
+                <TouchableOpacity style={[styles.checkBox,styles.marginLeft12]} onPress={handleStudentId} activeOpacity={0.7}>
                   <Text style={[styles.text11, styles.blueText]}>인증메일 발송</Text>
-                </Pressable>
+                </TouchableOpacity>
               </View>
             </View>
             <View style={[styles.rowView, styles.spacebetween]}>
@@ -191,7 +200,7 @@ const Password = ({ navigation }) => {
                       학번 인증 가이드 
                     </Text>
                 </Pressable>
-                <ErrorText isError={studentIdCheckError} errorMessage={studentIdCheckError}/>
+                <ErrorText isError={studentIdCheckError} errorMessage={studentIdCheckError} isChecked={isStudentIdValid} checkedMessage={studentIdCheckSuccess}/>
             </View>
             <View style={[styles.rowView, styles.margintop11]} >
               <View style = {styles.flex025}>
@@ -206,13 +215,13 @@ const Password = ({ navigation }) => {
                     setIsEmailNumberValid(false);
                   }}
                 />
-                <Pressable style={[styles.checkBox,styles.marginLeft12]} onPress={handleEmailCheck} disabled={!isEmailVerified}>
+                <TouchableOpacity style={[styles.checkBox,styles.marginLeft12]} onPress={handleEmailCheck} disabled={!isEmailVerified} activeOpacity={0.7}>
                   <Text style={[styles.text11, styles.blueText]}>인증번호 발송</Text>
-                </Pressable>
+                </TouchableOpacity>
               </View>
             </View>
            
-                <ErrorText isError={emailCheckError} errorMessage={emailCheckError}/>
+            <ErrorText isError={emailCheckError} errorMessage={emailCheckError} isChecked={isEmailNumberValid} checkedMessage={emailCheckSuccess}/>
            
             <View style={[styles.rowView, styles.margintop11]} >
                 <Text style={[styles.text12, styles.flex025]}>비밀번호</Text>
@@ -223,7 +232,7 @@ const Password = ({ navigation }) => {
                   secureTextEntry={true}
                 />
             </View>
-              <ErrorText isError={passwordError} errorMessage={passwordError}/>
+            <ErrorText isError={passwordError} errorMessage={passwordError} isChecked={passwordSuccess} checkedMessage={passwordSuccess}/>
             <View style={[styles.rowView, styles.margintop11]} >
                 <Text style={[styles.text12, styles.flex025]}>비밀번호 확인</Text>
                 <TextInput
@@ -233,7 +242,7 @@ const Password = ({ navigation }) => {
                   secureTextEntry={true}
                 />
             </View>
-              <ErrorText isError={passwordConfirmationError} errorMessage={passwordConfirmationError}/>
+            <ErrorText isError={passwordConfirmationError} errorMessage={passwordConfirmationError} isChecked={passwordConfirmation} checkedMessage={passwordConfirmationSuccess}/>
           </View>
             </KeyboardAwareScrollView>
             <ErrorText style={styles.marginRight20} isError={signupCheckError} errorMessage={signupCheckError}/>   
