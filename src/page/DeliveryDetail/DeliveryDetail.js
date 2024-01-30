58191348ba4a15b59b76a983c1677e4938dfdcb0
insu3160 <is23193160@gmail.com>
2024-01-30 18:45:39 +0900

    refactor: 시간 9시간 추가

--- src/page/DeliveryDetail/DeliveryDetail.js
+++ src/page/DeliveryDetail/DeliveryDetail.js
@@ -220,11 +220,11 @@ const DeliveryDetail = ({navigation, route}) => {
 	}
 
 	const DeliveryDetailCard = ({title, state, location, food, nickname, createAt, due, contents}) => {
-        const [now, setNow] = React.useState(moment().tz('Asia/Seoul'));
+        const [now, setNow] = React.useState(moment().tz('Asia/Seoul').add(9, 'hours'));
 		const [writeType, setWriteType] = React.useState('');
         React.useEffect(() => {
         const interval = setInterval(() => {
-            setNow(moment().tz('Asia/Seoul'));
+            setNow(moment().tz('Asia/Seoul').add(9, 'hours'));
         }, 60000); // 1분마다 갱신 (설정에 따라 조절 가능)
         
             return () => clearInterval(interval);
